675f20394a11ab7104d0a17161252e8a7168dd22
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-16 10:39:27 +0000

    gettings args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // get and check for args
+    let args: Vec<String> = env::args().collect();
+    if &args.len() != 2 {
+        panic!("Invalid number of arguments");
+    }
+    // get data folder path
+    let data_folder = &args[1];
+    // read folder
 }
