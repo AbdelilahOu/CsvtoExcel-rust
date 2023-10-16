e3c23e64cb9813d3100943b4524dcb0bc8a069fe
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-16 10:53:26 +0000

    checing for args with match

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::env;
+use std::{env::args, fs::read_dir};
 
 fn main() {
     // get and check for args
-    let args: Vec<String> = env::args().collect();
-    if &args.len() != 2 {
-        panic!("Invalid number of arguments");
+    let args: Vec<String> = args().collect();
+    // check for args
+    match &args.len() {
+        2 => {
+            // get data folder path
+            let data_folder = &args[1];
+            // read folder
+            let files = read_dir(data_folder).unwrap();
+            // iterate through files
+            for file in files {
+                println!("Name: {}", file.unwrap().path().display())
+            }
+        }
+        _ => panic!("Invalid number of arguments"),
     }
-    // get data folder path
-    let data_folder = &args[1];
-    // read folder
 }
